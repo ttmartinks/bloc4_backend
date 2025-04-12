9e934aa47ed1611deeffbd22f1564675b4021bc3
Martin D <martin.dubosq.lgm@gmail.com>
2025-04-12 18:07:11 +0200

    connect api & add testroute

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -7,7 +7,9 @@ router.get('/hello', (req, res) => {
     res.status(200).json({ message: 'Hello to the Cesizen\'s API!' });
   });
 
-
+router.get('/test', (req, res) => {
+    res.status(200).json({ message: 'Animi abutor pectus voluptatem tametsi. Votum appono venustas utroque vereor tabernus. Amita abbas amet suppono verbum. Et vulgus taceo aiunt claustrum quam. Cedo caterva utilis aut cupiditate atrox. Beatus caute recusandae pax ad, damnatio adicio sequi viduo video damno, dicta eligendi catena cursus aspicio velociter.' });
+  });
 
 
 module.exports = router;
