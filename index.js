1e97296ffb0d98820bfa831b746c347d96cd77c8
Martin D <martin.dubosq.lgm@gmail.com>
2025-06-26 19:39:06 +0200

    update other

--- index.js
+++
@@ -1,7 +0,0 @@
-var http =  require('http');
-require('dotenv').config();
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n' + process.env.PORT + '\n');
-}).listen(process.env.PORT);
